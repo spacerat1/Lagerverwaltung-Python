ac6dae8ab5afd3a8903ce24898ac481854a8d9a8
Pinke Bulette <spacerat1@gmx.de>
2025-04-01 12:49:23 +0200

    close db when program exits

--- Lagerverwaltung.py
+++ Lagerverwaltung.py
@@ -18,5 +18,7 @@ app = App(connection, cursor, user, path_to_db)
 app.window.state('zoomed')
 app.window.mainloop()
 #fc.add_standardmaterial(connection, cursor,(47203868, 'Modulträger ETSI 6x Modul 16x GfK 1:2sym', 'ST', 5, 20, 1))
+#fc.add_standardmaterial(connection, cursor,(40748416, 'nVent Kabelöse Stahl, 100 x 100', 'ST', 5, 20, 0))
 #fc.add_kleinstmaterial(connection, cursor,(40945101, 'nVent-Kontaktscheibe 6mm 100St.', 'ST', 0, 1, 0))
+connection.close()
 
