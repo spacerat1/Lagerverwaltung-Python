f5e1f3239669b0c05a7c7ecaa1db3aae7019a5b5
Pinke Bulette <spacerat1@gmx.de>
2025-04-09 19:03:34 +0200

    cleaned up the comments

--- Lagerverwaltung.py
+++ Lagerverwaltung.py
@@ -10,15 +10,16 @@ import application
 
 user = application.EXPERT
 
-# connect to database
-path_to_db: str = fc.open_db()
-connection: sqlite3.Connection = sqlite3.connect(path_to_db)
-connection.row_factory = sqlite3.Row  # set the standard return format
-cursor: sqlite3.Cursor = connection.cursor()
+if __name__ == '__main__':
+    # connect to database
+    path_to_db: str = fc.open_db()
+    connection: sqlite3.Connection = sqlite3.connect(path_to_db)
+    connection.row_factory = sqlite3.Row  # set the standard return format
+    cursor: sqlite3.Cursor = connection.cursor()
 
-# open the frontend
-app = application.App(connection, cursor, user, path_to_db)
-app.window.state("zoomed")
-app.window.mainloop()
-# fc.add_standardmaterial(connection, cursor,(40945101, 'nVent-Kontaktscheibe 6mm 100St.', 'ST', 0, 1, 0))
-connection.close()
+    # open the frontend
+    app = application.App(connection, cursor, user, path_to_db)
+    app.window.state("zoomed")
+    app.window.mainloop()
+    connection.close()
+
