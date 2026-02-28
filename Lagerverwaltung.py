729ead0f5e2186c9c945fa4c18b42369f9e41640
Pinke Bulette <spacerat1@gmx.de>
2026-02-28 16:55:40 +0100

    added function to write bundles

--- Lagerverwaltung.py
+++ Lagerverwaltung.py
@@ -23,8 +23,9 @@ if __name__ == '__main__':
     app.window.state("zoomed")
     app.window.mainloop()
     # fc.read_adresses_from_workorder_list(connection, cursor)
-    # fc.add_standardmaterial(connection, cursor, (47194948,'Gf-Atk 1x12F SC/APC9 1,5m SC/APC9 4m 20m', 'ST', 1, 5, False))
+    # fc.add_standardmaterial(connection, cursor, (47204338,'E&MMS 96 S/P BG für kombinierte ETSI HVt', 'ST', 1, 2, False))
     # fc.add_kleinstmaterial(connection, cursor,(40205824,'E&MMS-T EMK-Modul 8Rasteinheit 8Spleißk', 'ST', 2, 20, False))
+    # fc.add_bundle(connection, cursor, 40316599, 'PE E&MMS-L Gf HVt P/S-BGr 96 8m Langm G2', [40281900, 40296238])
     # fc.write_correction_data_from_yearly_inspection(connection, cursor)
     connection.close()
     
